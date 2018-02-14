368e974a624586a931339e7d6651ad3f0ba437de
aarondevon <me@aarondevon.com>
2018-02-14 08:54:25 -0600

    factorial

--- 02-factorialize-a-number.js
+++ 02-factorialize-a-number.js
@@ -15,8 +15,33 @@ factorialize(10) should return 3628800.
 factorialize(20) should return 2432902008176640000.
 factorialize(0) should return 1
 */
+
+// reverse loop solution
 function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  } 
+  for (let i = num - 1; i > 0; i--) {
+    console.log(num);
+    // console.log(i);
+    num *= i;
+  }
   return num;
+}
+
+factorialize(5);
+
+// normal loop solution
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  } 
+  let factorial = num;
+  for (let i = 1; i < num; i++) {
+    console.log(i);
+    factorial *= i;
+  }
+  return factorial;
 }
 
 factorialize(5);
